c36a9ce413f1b3044c794440e2123c4cf7ae9a09
igorfbairros <igorfbairros@gmail.com>
2021-08-27 00:25:35 -0300

    Add process name and email

--- ProgramAluno/src/thread/TelaTimeThread.java
+++ ProgramAluno/src/thread/TelaTimeThread.java
@@ -1,14 +1,22 @@
 package thread;
 
 import java.awt.BorderLayout;
+
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
 
 
 public class TelaTimeThread extends JDialog {
@@ -21,6 +29,51 @@ public class TelaTimeThread extends JDialog {
     private JLabel descricaoHora2 = new JLabel("Time Thread 2");
     private JTextField mostraTempo2 = new JTextField();
 
+    private JButton jButton = new JButton("Start");
+    private JButton jButton2 = new JButton("Stop");
+
+
+    private Runnable thread1 = new Runnable(){
+
+        @Override
+        public void run() {
+            while(true){
+                mostraTempo.setText(new SimpleDateFormat("dd/MM/yyyy hh:mm.ss").format(Calendar.getInstance().getTime()));
+                try{
+                    Thread.sleep(1000);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+     
+            
+        }
+        
+    }
+};
+///+++++++++++++++++++++++++ Division Threads 1 and 2
+    private Runnable thread2= new Runnable(){
+
+        @Override
+        public void run() {
+            while(true){
+                mostraTempo2.setText(new SimpleDateFormat("dd-MM-yyyy hh:mm:ss").format(Calendar.getInstance().getTime()));
+                try{
+                    Thread.sleep(1000);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+     
+            
+        }
+        
+    }
+};
+
+private Thread thread1Time;
+private Thread thread2Time;
+
+
+
     public TelaTimeThread(){ /*Executes whatever you have at the time of opening or execution */
         setTitle("Minha Tela de Time com Thread");
         setSize(new Dimension(240, 240));
@@ -31,6 +84,9 @@ public class TelaTimeThread extends JDialog {
         GridBagConstraints gridBagConstraints =  new GridBagConstraints(); /* Control position*/ 
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridwidth = 2; // occupies two place before the buttons
+        gridBagConstraints.insets = new Insets(5, 10, 5, 10);
+        gridBagConstraints.anchor = GridBagConstraints.WEST;
 
         descricaoHora.setPreferredSize(new Dimension(200, 25));
         gridBagConstraints.gridy ++ ;
@@ -38,6 +94,7 @@ public class TelaTimeThread extends JDialog {
 
         mostraTempo.setPreferredSize(new Dimension(200, 25));
         gridBagConstraints.gridy ++ ;
+        mostraTempo.setEditable(false);
         jPanel.add(mostraTempo, gridBagConstraints);
 
         descricaoHora2.setPreferredSize(new Dimension(200, 25));
@@ -47,7 +104,52 @@ public class TelaTimeThread extends JDialog {
 
         mostraTempo2.setPreferredSize(new Dimension(200, 25));
         gridBagConstraints.gridy ++;
+        mostraTempo2.setEditable(false);
         jPanel.add(mostraTempo2, gridBagConstraints);
+
+        gridBagConstraints.gridwidth = 1; // occupies a place before the buttons
+
+        jButton.setPreferredSize(new Dimension(92, 25));
+        gridBagConstraints.gridy ++ ;
+        jPanel.add(jButton, gridBagConstraints);
+
+        jButton2.setPreferredSize(new Dimension(92, 25));
+        gridBagConstraints.gridx ++ ;
+        jPanel.add(jButton2, gridBagConstraints);
+
+
+        jButton.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) { /*Executa clique do botão */
+                
+                thread1Time = new Thread(thread1);
+                thread1Time.start();
+
+                thread2Time = new Thread(thread2);
+                thread2Time.start();
+
+                jButton.setEnabled(false);
+                jButton2.setEnabled(true);
+                
+            }
+            
+        });
+
+        jButton2.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e){
+                thread1Time.stop();
+                
+                thread2Time.stop();
+
+                jButton.setEnabled(true);
+                jButton2.setEnabled(false);
+            }
+        });
+
+
         
         
 
@@ -56,7 +158,8 @@ public class TelaTimeThread extends JDialog {
         add(jPanel, BorderLayout.WEST);
         setVisible(true);/** Always gonna be the final  */
 
-    };
+    }
+}
 
     
-}
+
